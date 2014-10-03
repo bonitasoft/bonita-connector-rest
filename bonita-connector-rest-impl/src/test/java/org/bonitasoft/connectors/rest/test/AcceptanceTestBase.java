235e05ec8626ea69602f2090bd250cac7f629412
Pierrick Voulet <pierrick.voulet@bonitasoft.com>
2014-10-03 16:49:06 -0700

    REST services mocking is on the road (works but not configured for real tests)

---
+++ bonita-connector-rest-impl/src/test/java/org/bonitasoft/connectors/rest/test/AcceptanceTestBase.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2011 Thomas Akehurst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bonitasoft.connectors.rest.test;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.bonitasoft.engine.api.APIAccessor;
+import org.bonitasoft.engine.api.ProcessAPI;
+import org.bonitasoft.engine.connector.EngineExecutionContext;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import com.github.tomakehurst.wiremock.Log4jConfiguration;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.common.Log4jNotifier;
+import com.github.tomakehurst.wiremock.core.Options;
+
+public class AcceptanceTestBase {
+	protected EngineExecutionContext engineExecutionContext;
+	protected APIAccessor apiAccessor;
+	protected ProcessAPI processAPI;
+
+	protected static WireMockServer wireMockServer;
+	protected static int port = 8089;
+	
+	@BeforeClass
+	public static void setupServer() {
+		port = findFreePort(port);
+		wireMockServer = new WireMockServer(wireMockConfig().port(port).notifier(new Log4jNotifier()));
+		wireMockServer.start();
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public static void serverShutdown() {
+		wireMockServer.stop();
+	}
+
+	@Before
+	public void init() throws InterruptedException {
+		engineExecutionContext = mock(EngineExecutionContext.class);
+		apiAccessor = mock(APIAccessor.class);
+		processAPI = mock(ProcessAPI.class);
+		when(apiAccessor.getProcessAPI()).thenReturn(processAPI);
+		WireMock.configureFor("localhost", port);
+		Log4jConfiguration.configureLogging(true);
+		WireMock.reset();
+	}
+
+	private static int findFreePort(int myport) {
+		boolean free = false;
+		while (!free && myport <= 65535) {
+			if (isFreePort(myport)) {
+				free = true;
+			} else {
+				myport++;
+			}
+		}
+		return myport;
+	}
+
+	private static boolean isFreePort(final int myport) {
+		try {
+			final ServerSocket socket = new ServerSocket(myport);
+			socket.close();
+			return true;
+		} catch (final IOException e) {
+			return false;
+		}
+	}
+
+}
--- bonita-connector-rest-impl/src/test/java/org/bonitasoft/connectors/rest/test/RESTConnectorTest.java
+++ bonita-connector-rest-impl/src/test/java/org/bonitasoft/connectors/rest/test/RESTConnectorTest.java
@@ -14,123 +14,91 @@
 
 package org.bonitasoft.connectors.rest.test;
 
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeNotNull;
-import static org.junit.matchers.JUnitMatchers.containsString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import java.io.IOException;
-import java.net.ServerSocket;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.bonitasoft.connectors.rest.RESTConnector;
 import org.bonitasoft.connectors.rest.RESTResult;
-
-import org.bonitasoft.engine.api.APIAccessor;
-import org.bonitasoft.engine.api.ProcessAPI;
-import org.bonitasoft.engine.bpm.document.impl.DocumentImpl;
-import org.bonitasoft.engine.connector.EngineExecutionContext;
 import org.bonitasoft.engine.exception.BonitaException;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-public class RESTConnectorTest {
-    private EngineExecutionContext engineExecutionContext;
-    private APIAccessor apiAccessor;
-    private ProcessAPI processAPI;
+public class RESTConnectorTest extends AcceptanceTestBase {
+	private Map<String, Object> executeConnector(final Map<String, Object> parameters) throws BonitaException {
+		RESTConnector rest = new RESTConnector();
+		rest.setExecutionContext(engineExecutionContext);
+		rest.setAPIAccessor(apiAccessor);
+		rest.setInputParameters(parameters);
+		rest.validateInputParameters();
+		return rest.execute();
+	}
 
-    @Before
-    public void setUp() {
-        engineExecutionContext = mock(EngineExecutionContext.class);
-        apiAccessor = mock(APIAccessor.class);
-        processAPI = mock(ProcessAPI.class);
-        when(apiAccessor.getProcessAPI()).thenReturn(processAPI);
-    }
+	private Map<String, Object> getGetSettings() {
+		final Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("url", "http://localhost:" + port + "/");
+		parameters.put("method", "GET");
+		parameters.put("contentType", "application/json");
+		parameters.put("charset", "UTF-8");
+		parameters.put("urlCookies", new ArrayList<String>());
+		parameters.put("urlHeaders", new ArrayList<String>());
+		parameters.put("body", "");
+		return parameters;
+	}
 
-    private Map<String, Object> executeConnector(final Map<String, Object> parameters) throws BonitaException {
-        RESTConnector rest = new RESTConnector();
-        rest.setExecutionContext(engineExecutionContext);
-        rest.setAPIAccessor(apiAccessor);
-        rest.setInputParameters(parameters);
-        rest.validateInputParameters();
-        return rest.execute();
-    }
+	private Map<String, Object> getPostSettings() {
+		final Map<String, Object> parameters = new HashMap<String, Object>();
+		return parameters;
+	}
 
-    private Map<String, Object> getGetSettings() {
-        final Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("url", "https://api.github.com/orgs/bonitasoft");
-        parameters.put("method", "GET");
-        parameters.put("contentType", "text/plain");
-        parameters.put("charset", "UTF-8");
-        parameters.put("urlCookies", new ArrayList<String>());
-        parameters.put("urlHeaders", new ArrayList<String>());
-        parameters.put("body", "");
-        return parameters;
-    }
-    
-    private Map<String, Object> getPostSettings() {
-        final Map<String, Object> parameters = new HashMap<String, Object>();
-        return parameters;
-    }
+	private Map<String, Object> getPutSettings() {
+		final Map<String, Object> parameters = new HashMap<String, Object>();
+		return parameters;
+	}
 
-    private Map<String, Object> getPutSettings() {
-        final Map<String, Object> parameters = new HashMap<String, Object>();
-        return parameters;
-    }
+	private Map<String, Object> getDeleteSettings() {
+		final Map<String, Object> parameters = new HashMap<String, Object>();
+		return parameters;
+	}
 
-    private Map<String, Object> getDeleteSettings() {
-        final Map<String, Object> parameters = new HashMap<String, Object>();
-        return parameters;
-    }
-    
+	@Test
+	public void sendGetRESTRequest() throws BonitaException, InterruptedException {
+		stubFor(get(urlEqualTo("/"))
+				.willReturn(aResponse()
+						.withStatus(200)
+						.withHeader("Content-Type", "text/plain")
+						.withBody("bonita")));
 
-    @Test
-    public void sendGetRESTRequest() throws BonitaException, InterruptedException {
-        Map<String, Object> restResult = executeConnector(getGetSettings());
-        assertEquals(restResult.size(), 1);
-        assertNotNull(restResult.get("result"));
-        Object result = restResult.get("result");
-        assertTrue(result instanceof RESTResult);
-        RESTResult restResultContent = (RESTResult) result;
-        assertEquals(200, restResultContent.getStatusCode());
-        assertTrue(restResultContent.getEntity().contains("bonitasoft"));
-    }
-    
-    @Test
-    public void sendPostRESTRequest() throws BonitaException, InterruptedException {
-    	Map<String, Object> restResult = executeConnector(getPostSettings());
-        assertEquals(restResult.size(), 1);
-        assertNotNull(restResult.get("result"));
-        Object result = restResult.get("result");
-        assertTrue(result instanceof RESTResult);
-        RESTResult restResultContent = (RESTResult) result;
-        assertEquals(200, restResultContent.getStatusCode());
-        assertTrue(restResultContent.getEntity().contains("bonitasoft"));
-    }
-    
-    @Test
-    public void sendPutRESTRequest() throws BonitaException, InterruptedException {
-        Map<String, Object> restResult = executeConnector(getPutSettings());
-        assertEquals(1, 1);
-    }
-    
-    @Test
-    public void sendDeleteRESTRequest() throws BonitaException, InterruptedException {
-        Map<String, Object> restResult = executeConnector(getDeleteSettings());
-        assertEquals(1, 1);
-    }
+		Map<String, Object> restResult = executeConnector(getGetSettings());
+		assertEquals(restResult.size(), 1);
+		assertNotNull(restResult.get("result"));
+		Object result = restResult.get("result");
+		assertTrue(result instanceof RESTResult);
+		RESTResult restResultContent = (RESTResult) result;
+		assertEquals(200, restResultContent.getStatusCode());
+		assertTrue(restResultContent.getEntity().contains("bonitasoft"));
+	}
+
+//	@Test
+//	public void sendPostRESTRequest() throws BonitaException, InterruptedException {
+//		assertTrue(true);
+//	}
+//
+//	@Test
+//	public void sendPutRESTRequest() throws BonitaException, InterruptedException {
+//		assertTrue(true);
+//	}
+//
+//	@Test
+//	public void sendDeleteRESTRequest() throws BonitaException, InterruptedException {
+//		assertTrue(true);
+//	}
 }
