48bb96809d534abefcc072ec9461b84a8ca695d8
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2018-04-30 15:19:08 +0200

    chore(ci): use New CI pipeline syntax for release
    Covers [CI-275](https://bonitasoft.atlassian.net/browse/CI-275)

--- infrastructure/release.groovy
+++ infrastructure/release.groovy
@@ -1,10 +1,13 @@
-def mvn(args) {
-    sh "${tool 'maven'}/bin/mvn ${args}"
-}
+timestamps {
+    ansiColor('xterm') {
+        node {
+            stage('Setup') {
+                checkout scm
+            }
 
-node {
-    checkout scm
-
-    stage('release version')
-    mvn 'release:prepare -B'
+            stage('Release') {
+                sh './mvnw release:prepare -B'
+            }
+        }
+    }
 }
