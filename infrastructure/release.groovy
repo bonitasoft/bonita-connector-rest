7ff52a3983f2bacb29178c8c514596a387d7fd8d
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-08-23 14:00:09 +0200

    Update release.groovy

--- infrastructure/release.groovy
+++ infrastructure/release.groovy
@@ -1,23 +1,18 @@
 timestamps {
     ansiColor('xterm') {
         node {
-            stage('Setup') {
-                configGitCredentialHelper()
-                checkout scm
-            }
+              withCredentials([usernamePassword(
+                    credentialsId: 'github',
+                    passwordVariable: 'GIT_PASSWORD',
+                    usernameVariable: 'GIT_USERNAME')]) {
+                stage('Setup') {
+                    checkout scm
+                }
 
-            stage('Release') {
-                sh './mvnw release:prepare -B'
+                stage('Release') {
+                    sh './mvnw release:prepare -B'
+                }
             }
         }
     }
-}
-
-def configGitCredentialHelper() {
-    sh """#!/bin/bash +x
-        set -e
-        echo "Using the git cache credential helper to be able to perform native git commands without passing authentication parameters"
-        # Timeout in seconds, ensure we have enough time to perform the whole process between the initial clone and the final branch push
-        git config --global credential.helper 'cache --timeout=18000'
-    """    
 }
