265b142df5c6c315d2d4be19e69ce41c7be68100
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-08-23 14:04:20 +0200

    Update release.groovy

--- infrastructure/release.groovy
+++ infrastructure/release.groovy
@@ -1,18 +1,16 @@
 timestamps {
-    ansiColor('xterm') {
         node {
-              withCredentials([usernamePassword(
-                    credentialsId: 'github',
-                    passwordVariable: 'GIT_PASSWORD',
-                    usernameVariable: 'GIT_USERNAME')]) {
                 stage('Setup') {
                     checkout scm
                 }
 
                 stage('Release') {
-                    sh './mvnw release:prepare -B'
+                       withCredentials([usernamePassword(
+                            credentialsId: 'github',
+                            passwordVariable: 'GIT_PASSWORD',
+                            usernameVariable: 'GIT_USERNAME')]) {
+                                sh './mvnw release:prepare -B'
+                       }
                 }
             }
-        }
-    }
 }
