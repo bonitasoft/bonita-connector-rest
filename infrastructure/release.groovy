19a573890b1a7161ab72427a5f36676923fb9298
Bonita CI <bonita-ci@bonitasoft.com>
2018-08-30 10:29:57 +0200

    tr(build) fix release job

--- infrastructure/release.groovy
+++ infrastructure/release.groovy
@@ -9,7 +9,7 @@ timestamps {
                             credentialsId: 'github',
                             passwordVariable: 'GIT_PASSWORD',
                             usernameVariable: 'GIT_USERNAME')]) {
-                                sh './mvnw release:prepare -B'
+                                sh './mvnw release:prepare release:perform -B -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_SNAPSHOTS}'
                        }
                 }
             }
